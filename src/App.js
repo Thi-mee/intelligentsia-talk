b7473270a4aae7459e8f1e7987c9c79762d1a36d
Timilehin Adenuga <timswaggs1@outlook.com>
2022-11-04 16:25:45 +0100

    saved for re run from local host

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-// import ReactPlayer from 'react-player'
-import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Nav from './components/Nav';
 import Rain from './components/Rain';
@@ -15,30 +14,27 @@ import Speakers from './pages/Speakers';
 function App() {
   return (
     <Router>
-       <Rain/>
-        <div className="App">
-            <Nav/>
-            <div className="entire-body">
-              <Switch>
-                  <Route exact path="/">
-                     <Home/>
-                  </Route>
-                  <Route path="/About">
-                      <About/>
-                  </Route>
-                  <Route path="/Contact">
-                      <Contact/>
-                  </Route>
-                  <Route path="/Speakers">
-                      <Speakers/>
-                  </Route>
-              </Switch>
-            </div>
-            {/* <div className="react-player-holder">
-              <ReactPlayer width="100%" height="100%" url="https://www.youtube-nocookie.com/embed/KzHzGCSPED4"/>
-            </div> */}
-            <Footer/>
+      <Rain />
+      <div className="App">
+        <Nav />
+        <div className="entire-body">
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/About">
+              <About />
+            </Route>
+            <Route path="/Contact">
+              <Contact />
+            </Route>
+            <Route path="/Speakers">
+              <Speakers />
+            </Route>
+          </Switch>
         </div>
+        <Footer />
+      </div>
     </Router>
   );
 }
--- src/Home/Sugestedvideos.js
+++ src/Home/Sugestedvideos.js
@@ -3,8 +3,8 @@ import Homevideos from "./Homevideos";
 const Suggestedvideos = () => {
   return (
     <div className="Suggestedvideos">
-      <div className="container">
-        <div className="sub_head_suggested_videos">
+      <>
+        <div className="sub_head_suggested_videos container">
           <h3 className="know-more">Suggested Videos</h3>
         </div>
         <div className="videos_list">
@@ -12,7 +12,7 @@ const Suggestedvideos = () => {
             <Homevideos />
           </div>
         </div>
-      </div>
+      </>
     </div>
   );
 };
